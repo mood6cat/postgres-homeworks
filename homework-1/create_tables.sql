3e80f48a6b54c8ce7b463913b6e26b887d06ae06
dant9 <dant982@mail.ru>
2023-08-12 21:51:22 +0500

    Ошибочки

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,56 @@
 -- SQL-команды для создания таблиц
+-- Table: public.customers
+
+-- DROP TABLE IF EXISTS public.customers;
+
+CREATE TABLE IF NOT EXISTS public.customers
+(
+    customer_id character varying(6) COLLATE pg_catalog."default" NOT NULL,
+    company_name character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    contact_name character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT customers_pkey PRIMARY KEY (customer_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.customers
+    OWNER to postgres;
+
+-- Table: public.employes
+
+-- DROP TABLE IF EXISTS public.employes;
+
+CREATE TABLE IF NOT EXISTS public.employes
+(
+    employee_id integer NOT NULL,
+    first_name character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    title character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    birth_date character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    notes text COLLATE pg_catalog."default",
+    CONSTRAINT employes_pkey PRIMARY KEY (employee_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.employes
+    OWNER to postgres;
+
+-- Table: public.orders
+
+-- DROP TABLE IF EXISTS public.orders;
+
+CREATE TABLE IF NOT EXISTS public.orders
+(
+    order_id integer NOT NULL,
+    customer_id character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    employee_id integer NOT NULL,
+    order_date character varying(11) COLLATE pg_catalog."default",
+    ship_city character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT orders_pkey PRIMARY KEY (order_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.orders
+    OWNER to postgres;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,74 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+# import psycopg2
+#
+# #conn to db Вместе с менеджером контекста
+# conn = psycopg2.connect(
+#     host='localhost',
+#     database='north',
+#     user='postgres',
+#     password="84627913"
+# )
+# try:
+#     with conn:
+#         # create cursor для различных операций с БД
+#         with conn.cursor() as cur:
+#
+#             # execute some query делем запрос
+#             execute_some = cur.execute("")
+# conn.close()
+
+import psycopg2
+import pandas
+import os
+
+
+
+
+def build_path(file: str) -> str:
+    """
+    Возвращает путь от корня проекта к указанному файлу в папке 'north_data'.
+
+    file: название файла
+    """
+
+    path_to_file = os.path.join(os.path.dirname(__file__), 'north_data', file)
+    return path_to_file
+
+
+def add_data_to_table(table: str, data: pandas.DataFrame, cursor: psycopg2.extensions.cursor):
+    """
+    Добавляет переданные данные в указанную таблицу базы данных.
+
+    table: название таблицы в базе данных
+    data: данные для записи в таблицу
+    cursor: объект для взаимодействия с базой данных
+    """
+
+    cursor.execute(f"TRUNCATE TABLE {table} CASCADE")
+
+    columns = data.columns.tolist()
+    string_placeholders = ", ".join(["%s"] * len(columns))
+    command = f"INSERT INTO {table} VALUES ({string_placeholders})"
+
+    for index, row in data.iterrows():
+        values = [row[column] for column in columns]
+        cursor.execute(command, tuple(values))
+
+
+if __name__ == '__main__':
+    customers = pandas.read_csv(build_path('customers_data.csv'))
+    employees = pandas.read_csv(build_path('employees_data.csv'))
+    orders = pandas.read_csv(build_path('orders_data.csv'))
+
+    with psycopg2.connect(
+            host='localhost',
+            database='north',
+            user='postgres',
+            pasword='84627913'
+    ) as conn:
+        with conn.cursor() as cur:
+            add_data_to_table("customers", customers, cur)
+            add_data_to_table("employees", employees, cur)
+            add_data_to_table("orders", orders, cur)
+
+    conn.close()
